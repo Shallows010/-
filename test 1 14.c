7251b8ae20806dc14c6e4555e90b500cf70a5c68
Shallows010 <155981709+Shallows010@users.noreply.github.com>
2024-01-14 19:46:27 +0800

    今日教学练习

---
+++ test 1 14.c
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int input,i;
+//
+//	
+//		printf("你要好好学习吗?(1/2)\n");
+//		scanf("%d", &input);
+//		if (input == 1)
+//			printf("offer");
+//		else
+//			printf("回家种地");
+//	
+//	
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	while (line < 30000)
+//	{
+//		printf("\n写一行代码:%d\n",line);
+//		line++;
+//	}
+//	if (line == 30000)
+//		printf("\n跳出\n");
+//
+//	return 0;
+//}
+//int Add(int x,int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0, num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = Add(num1, num2);
+//	printf("%d\n",sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	char ch[5] = { 'a','b','c'};//不完全初始化，剩余默认为0
+//	int i = 0, s = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("arr[%d] = %d\n", s, arr[i]);
+//		s = s + 1;
+//	}
+//	i = 0, s = 0;
+//	while (i < 10)
+//	{
+//		printf("arr[%d] = %d\n", s, arr[i]);
+//		s = s + 1;
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 2;
+//	a = a + 5;//a += 5;
+//	a = a - 3;//a -= 3;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", !a);
+//	if (a)
+//	{
+//		//如果a为真，输入
+//	}
+//	if (!a)
+//	{
+//		//如果a为假，输入
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	//10 * 4 = 40(字节)
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr));//计算数组的总大小，单位是字节
+//	printf("%d\n", sizeof(arr[0]));
+//	int se = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", se);
+//	return 0;
+//}
+int main()
+{
+	int a = 1;
+	printf("%d\n", a);
+
+	return 0;
+}
